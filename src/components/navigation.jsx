1fbae45636407fa92700eddd8e6bc56b737b581c
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-23 18:10:09 +0330

    Active link

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 class Navbar extends Component {
   state = {};
   render() {
     return (
       <>
-        <nav className="navbar navbar-expand-lg navbar-primary bg-primary px-4">
-          <Link className="navbar-brand" to="/">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
+          <NavLink className="navbar-brand" to="/">
             Home
-          </Link>
+          </NavLink>
           <button
             className="navbar-toggler"
             type="button"
@@ -23,20 +23,20 @@ class Navbar extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/users">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/users">
                   Users
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/login">
+                <NavLink className="nav-link" to="/login">
                   Login
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/register">
+                <NavLink className="nav-link" to="/register">
                   Register
-                </Link>
+                </NavLink>
               </li>
             </ul>
           </div>
